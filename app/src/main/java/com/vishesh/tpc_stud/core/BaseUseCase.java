bdcf18f0b527bea8be09d958d2122fcc4fc6fed9
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-02-14 23:31:24 +0530

    Create BaseUseCase class

--- app/src/main/java/com/vishesh/tpc_stud/core/BaseUseCase.java
+++ app/src/main/java/com/vishesh/tpc_stud/core/BaseUseCase.java
@@ -1,12 +1,47 @@
 package com.vishesh.tpc_stud.core;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * Created by vishesh on 14/2/17.
  */
 public abstract class BaseUseCase<T, Parameters> {
 
-    
+    private final Scheduler jobScheduler;
+    private final Scheduler postJobScheduler;
+    private final CompositeDisposable compositeDisposable;
 
+    @Inject
+    protected BaseUseCase(Scheduler jobScheduler,
+                          Scheduler postJobScheduler,
+                          CompositeDisposable compositeDisposable) {
+        this.jobScheduler = jobScheduler;
+        this.postJobScheduler = postJobScheduler;
+        this.compositeDisposable = compositeDisposable;
+    }
+
+    public void execute(DisposableObserver<T> disposableObserver, Parameters params) {
+        Observable<T> observable = buildObservable(params)
+                .subscribeOn(jobScheduler)
+                .observeOn(postJobScheduler);
+        addDisposable(observable.subscribeWith(disposableObserver));
+    }
+
+    public void dispose() {
+        if (!compositeDisposable.isDisposed()) {
+            compositeDisposable.dispose();
+        }
+    }
+
+    protected abstract Observable<T> buildObservable(Parameters params);
+
+    private void addDisposable(Disposable disposable) {
+        compositeDisposable.add(disposable);
+    }
 }
