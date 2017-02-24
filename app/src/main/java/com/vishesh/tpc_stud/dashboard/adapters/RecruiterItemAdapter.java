6e0878c350c606c313c1b2778d2c48521fe056c1
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-02-24 20:06:53 +0530

    Layout for recruiter cardView

--- app/src/main/java/com/vishesh/tpc_stud/dashboard/adapters/RecruiterItemAdapter.java
+++ app/src/main/java/com/vishesh/tpc_stud/dashboard/adapters/RecruiterItemAdapter.java
@@ -1,31 +1,43 @@
 package com.vishesh.tpc_stud.dashboard.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fernandocejas.arrow.optional.Optional;
+import com.vishesh.tpc_stud.R;
 import com.vishesh.tpc_stud.dashboard.models.RecruiterModel;
 
 import java.util.List;
 
-public class RecruiterItemAdapter extends RecyclerView.Adapter<RecruiterItemAdapter.ViewHolder> {
+import javax.inject.Inject;
 
+public class RecruiterItemAdapter extends RecyclerView.Adapter<RecruiterItemAdapter.ViewHolder> {
 
     private Optional<List<RecruiterModel>> recruitersOptional;
 
-    public RecruiterItemAdapter() {
+    private final Context context;
 
+    @Inject
+    public RecruiterItemAdapter(Context context) {
+        this.context = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_recruiter_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
+        if(recruitersOptional.isPresent()){
+            List<RecruiterModel> recruiterModels = recruitersOptional.get();
+
+        }
     }
 
     public void setData(List<RecruiterModel> recruiterModels){
@@ -38,7 +50,7 @@ public class RecruiterItemAdapter extends RecyclerView.Adapter<RecruiterItemAdap
         return recruitersOptional.isPresent() ? recruitersOptional.get().size() : 0;
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
 
         public ViewHolder(View itemView) {
             super(itemView);
