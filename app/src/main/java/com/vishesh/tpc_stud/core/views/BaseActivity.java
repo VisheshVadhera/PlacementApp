0165ccee011970e581f7b9ba4e6cf24ff5c2d38b
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-02-24 12:39:31 +0530

    Change fragmentManager to supportFragmentManager

--- app/src/main/java/com/vishesh/tpc_stud/core/views/BaseActivity.java
+++ app/src/main/java/com/vishesh/tpc_stud/core/views/BaseActivity.java
@@ -1,8 +1,8 @@
 package com.vishesh.tpc_stud.core.views;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.vishesh.tpc_stud.core.AppComponent;
@@ -20,7 +20,7 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     protected void addFragment(int containerViewId, Fragment fragment) {
-        getFragmentManager()
+        getSupportFragmentManager()
                 .beginTransaction()
                 .add(containerViewId, fragment)
                 .commit();
