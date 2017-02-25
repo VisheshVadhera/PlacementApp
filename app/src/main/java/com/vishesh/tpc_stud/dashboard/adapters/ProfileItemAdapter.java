1919aac92b867b7a08ff7e4cd367f848a7a739ee
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-02-25 21:51:35 +0530

    Calling delegates from profile adapter

--- app/src/main/java/com/vishesh/tpc_stud/dashboard/adapters/ProfileItemAdapter.java
+++ app/src/main/java/com/vishesh/tpc_stud/dashboard/adapters/ProfileItemAdapter.java
@@ -1,47 +1,102 @@
 package com.vishesh.tpc_stud.dashboard.adapters;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.view.ViewGroup;
 
-import javax.inject.Inject;
+import com.vishesh.tpc_stud.core.models.User;
+import com.vishesh.tpc_stud.dashboard.models.UserProfile;
 
-import butterknife.ButterKnife;
+import javax.inject.Inject;
 
 /**
  * Created by vishesh on 24/2/17.
  */
 
-public class ProfileItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class ProfileItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private final Context context;
+    private static final int VIEW_TYPE_HEADER = 0;
+    private static final int VIEW_TYPE_GPA = 1;
+    private static final int VIEW_TYPE_CV = 2;
+    private static final int VIEW_TYPE_COURSE = 3;
+    private static final int VIEW_TYPE_NETWORK_PROFILE = 4;
+
+    private final HeaderAdapterDelegate headerAdapterDelegate;
+    private final GpaAdapterDelegate gpaAdapterDelegate;
+    private final CvAdapterDelegate cvAdapterDelegate;
+    private final CourseAdapterDelegate courseAdapterDelegate;
+    private final NetworkProfileAdapter networkProfileAdapter;
+
+    private User user;
+    private UserProfile userProfile;
 
     @Inject
-    public ProfileItemAdapter(Context context) {
-        this.context = context;
+    public ProfileItemAdapter(HeaderAdapterDelegate headerAdapterDelegate,
+                              GpaAdapterDelegate gpaAdapterDelegate,
+                              CvAdapterDelegate cvAdapterDelegate,
+                              CourseAdapterDelegate courseAdapterDelegate,
+                              NetworkProfileAdapter networkProfileAdapter) {
+        this.headerAdapterDelegate = headerAdapterDelegate;
+        this.gpaAdapterDelegate = gpaAdapterDelegate;
+        this.cvAdapterDelegate = cvAdapterDelegate;
+        this.courseAdapterDelegate = courseAdapterDelegate;
+        this.networkProfileAdapter = networkProfileAdapter;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(context).inflate(, parent, false);
-        return null;
+        switch (viewType) {
+            case VIEW_TYPE_HEADER:
+                return headerAdapterDelegate.onCreateViewHolder(parent);
+            case VIEW_TYPE_GPA:
+                return gpaAdapterDelegate.onCreateViewHolder(parent);
+            case VIEW_TYPE_CV:
+                return cvAdapterDelegate.onCreateViewHolder(parent);
+            case VIEW_TYPE_COURSE:
+                return courseAdapterDelegate.onCreateViewHolder(parent);
+            default:
+                return networkProfileAdapter.onCreateViewHolder(parent);
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_HEADER:
+                headerAdapterDelegate.onBindViewHolder(user, position, holder, null);
+                break;
+            case VIEW_TYPE_GPA:
+                gpaAdapterDelegate.onBindViewHolder(userProfile, position, holder, null);
+                break;
+            case VIEW_TYPE_CV:
+                cvAdapterDelegate.onBindViewHolder(userProfile, position, holder, null);
+                break;
+            case VIEW_TYPE_COURSE:
+                courseAdapterDelegate.onBindViewHolder(userProfile, position, holder, null);
+                break;
+            default:
+                networkProfileAdapter.onBindViewHolder(userProfile.getNetworkProfiles(),
+                        position, holder, null);
+                break;
+        }
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return VIEW_TYPE_HEADER;
+        } else if (position == 1) {
+            return VIEW_TYPE_GPA;
+        } else if (position == 2) {
+            return VIEW_TYPE_CV;
+        } else if (position == 3) {
+            return VIEW_TYPE_COURSE;
+        } else {
+            return VIEW_TYPE_NETWORK_PROFILE;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-        }
+        return userProfile.getNetworkProfiles().size() + 4;
     }
 }
