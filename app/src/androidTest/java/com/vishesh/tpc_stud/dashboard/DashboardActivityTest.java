6d86dfa5c4debd7a3cddc58763ea8a5e807d1a19
Vishesh <vishesh.2710@gmail.com>
2017-03-07 14:47:54 +0530

    Add menu clicked test

--- app/src/androidTest/java/com/vishesh/tpc_stud/dashboard/DashboardActivityTest.java
+++ app/src/androidTest/java/com/vishesh/tpc_stud/dashboard/DashboardActivityTest.java
@@ -1,5 +1,6 @@
 package com.vishesh.tpc_stud.dashboard;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class DashboardActivityTest {
@@ -28,5 +37,17 @@ public class DashboardActivityTest {
 
         Assertions.assertThat(fragment)
                 .isNotNull();
+    }
+
+    @Test
+    public void onMenuClicked_showLogoutOptionInOverflowMenu() {
+
+        onView(withId(R.id.toolbar))
+                .perform(click());
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        onView(withText(R.string.action_logout))
+                .check(matches(isDisplayed()));
     }
 }
