75dc7fc2cba40d7ff60f4c5bacb0c45833fef22c
Vishesh <vishesh.2710@gmail.com>
2017-03-16 13:07:18 +0530

    Add FakeaccountKitLoginResult

---
+++ app/src/androidTest/java/com/vishesh/tpc_stud/auth/FakeAccountKitLoginResult.java
@@ -0,0 +1,100 @@
+package com.vishesh.tpc_stud.auth;
+
+import android.os.Parcel;
+import android.support.annotation.Nullable;
+
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+
+class FakeAccountKitLoginResult implements AccountKitLoginResult {
+
+    private final AccessToken accessToken;
+    private final String authorizationCode;
+    private final boolean cancelled;
+    private final AccountKitError error;
+    private final String finalAuthorizationState;
+    private final long tokenRefreshIntervalInSeconds;
+
+    public static final Creator<FakeAccountKitLoginResult> CREATOR = new Creator() {
+        public FakeAccountKitLoginResult createFromParcel(Parcel source) {
+            return new FakeAccountKitLoginResult(source);
+        }
+
+        public FakeAccountKitLoginResult[] newArray(int size) {
+            return new FakeAccountKitLoginResult[size];
+        }
+    };
+
+    private FakeAccountKitLoginResult(AccessToken accessToken,
+                                      String authorizationCode,
+                                      boolean cancelled,
+                                      AccountKitError error,
+                                      String finalAuthorizationState,
+                                      long tokenRefreshIntervalInSeconds) {
+        this.accessToken = accessToken;
+        this.authorizationCode = authorizationCode;
+        this.cancelled = cancelled;
+        this.error = error;
+        this.finalAuthorizationState = finalAuthorizationState;
+        this.tokenRefreshIntervalInSeconds = tokenRefreshIntervalInSeconds;
+    }
+
+    @Nullable
+    @Override
+    public AccessToken getAccessToken() {
+        return accessToken;
+    }
+
+    @Nullable
+    @Override
+    public String getAuthorizationCode() {
+        return authorizationCode;
+    }
+
+    @Nullable
+    @Override
+    public AccountKitError getError() {
+        return error;
+    }
+
+    @Nullable
+    @Override
+    public String getFinalAuthorizationState() {
+        return finalAuthorizationState;
+    }
+
+    @Override
+    public long getTokenRefreshIntervalInSeconds() {
+        return tokenRefreshIntervalInSeconds;
+    }
+
+    @Override
+    public boolean wasCancelled() {
+        return this.error == null && this.authorizationCode == null && this.accessToken == null;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.accessToken, flags);
+        dest.writeString(this.authorizationCode);
+        dest.writeString(this.finalAuthorizationState);
+        dest.writeLong(this.tokenRefreshIntervalInSeconds);
+        dest.writeParcelable(this.error, flags);
+        dest.writeByte((byte)(this.cancelled?1:0));
+    }
+
+    private FakeAccountKitLoginResult(Parcel parcel) {
+        this.accessToken = (AccessToken)parcel.readParcelable(AccessToken.class.getClassLoader());
+        this.authorizationCode = parcel.readString();
+        this.finalAuthorizationState = parcel.readString();
+        this.tokenRefreshIntervalInSeconds = parcel.readLong();
+        this.error = (AccountKitError)parcel.readParcelable(AccountKitError.class.getClassLoader());
+        this.cancelled = parcel.readByte() == 1;
+    }
+}
--- app/src/androidTest/java/com/vishesh/tpc_stud/auth/LoginActivityTest.java
+++ app/src/androidTest/java/com/vishesh/tpc_stud/auth/LoginActivityTest.java
@@ -1,9 +1,12 @@
 package com.vishesh.tpc_stud.auth;
 
-import android.support.test.rule.ActivityTestRule;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.app.Fragment;
 
+import com.facebook.accountkit.ui.AccountKitActivity;
 import com.vishesh.tpc_stud.R;
 import com.vishesh.tpc_stud.auth.views.LoginActivity;
 
@@ -11,9 +14,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.Instrumentation.ActivityResult;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,13 +28,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
 
+    /**
+     * Testing Strategy:
+     * <p>
+     * <p>
+     * For login flow, partition the state space as follows:
+     * <p>
+     * Successful login and no cancellation by the user,
+     * Successful login and cancellation by the user,
+     * Unsuccessful login.
+     */
+
     @Rule
-    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule =
-            new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    public IntentsTestRule<LoginActivity> loginActivityIntentsTestRule =
+            new IntentsTestRule<LoginActivity>(LoginActivity.class);
 
     @Test
     public void testContainsLoginFragment() {
-        Fragment loginFragment = loginActivityActivityTestRule
+        Fragment loginFragment = loginActivityIntentsTestRule
                 .getActivity()
                 .getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_container);
@@ -37,11 +54,30 @@ public class LoginActivityTest {
                 .isNotNull();
     }
 
+    /**
+     * Covers the following case:
+     * Successful login and no cancellation by the user
+     */
     @Test
     public void clickLoginButton_openAccountKitActivity() {
 
         onView(withId(R.id.button_login)).perform(click());
 
         onView(withId(R.id.com_accountkit_background)).check(matches(isDisplayed()));
+
+        ActivityResult activityResult = createAccountKitActivitySuccessfulLoginResultStub();
+
+        intending(hasExtraWithKey(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION))
+                .respondWith(activityResult);
+
+
     }
+
+    private ActivityResult createAccountKitActivitySuccessfulLoginResultStub() {
+        Intent intent = new Intent();
+//        AccountKitLoginResult accountKitLoginResult =
+//        intent.putExtra()
+        return new ActivityResult(Activity.RESULT_OK, intent);
+    }
+
 }
