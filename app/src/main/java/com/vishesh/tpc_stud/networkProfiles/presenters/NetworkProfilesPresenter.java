75a3547757a23ccfd2b60335b1b6e00bfd14d4d9
Vishesh <vishesh.2710@gmail.com>
2017-03-18 17:42:54 +0530

    Add unit test for networkProfilesPresenter's onStart

--- app/src/main/java/com/vishesh/tpc_stud/networkProfiles/presenters/NetworkProfilesPresenter.java
+++ app/src/main/java/com/vishesh/tpc_stud/networkProfiles/presenters/NetworkProfilesPresenter.java
@@ -1,21 +1,30 @@
 package com.vishesh.tpc_stud.networkProfiles.presenters;
 
 import com.vishesh.tpc_stud.core.presenters.BasePresenter;
+import com.vishesh.tpc_stud.core.repos.LocalCache;
 import com.vishesh.tpc_stud.core.views.BaseView;
+import com.vishesh.tpc_stud.dashboard.models.NetworkProfile;
 import com.vishesh.tpc_stud.networkProfiles.useCases.GetNetworkProfilesUseCase;
 
+import java.util.List;
+
 import javax.inject.Inject;
+
+import io.reactivex.observers.DisposableSingleObserver;
 
 
 public class NetworkProfilesPresenter extends BasePresenter {
 
     private final GetNetworkProfilesUseCase getNetworkProfilesUseCase;
+    private final LocalCache localCache;
 
     private NetworkProfilesView networkProfilesView;
 
     @Inject
-    public NetworkProfilesPresenter(GetNetworkProfilesUseCase getNetworkProfilesUseCase) {
+    public NetworkProfilesPresenter(GetNetworkProfilesUseCase getNetworkProfilesUseCase,
+                                    LocalCache localCache) {
         this.getNetworkProfilesUseCase = getNetworkProfilesUseCase;
+        this.localCache = localCache;
     }
 
     @Override
@@ -39,10 +48,26 @@ public class NetworkProfilesPresenter extends BasePresenter {
 
     public void onStart() {
         networkProfilesView.showLoader();
-
+        getNetworkProfilesUseCase.execute(new NetworkProfilesObserver(),
+                localCache.getUserId(), new Object());
     }
 
     public interface NetworkProfilesView extends BaseView {
 
+    }
+
+    private final class NetworkProfilesObserver extends DisposableSingleObserver<List<NetworkProfile>> {
+
+        @Override
+        public void onSuccess(List<NetworkProfile> networkProfiles) {
+            networkProfilesView.hideLoader();
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            networkProfilesView.hideLoader();
+            handleError(e);
+        }
     }
 }
--- app/src/test/java/com/vishesh/tpc_stud/networkProfiles/presenters/NetworkProfilesPresenterTest.java
+++ app/src/test/java/com/vishesh/tpc_stud/networkProfiles/presenters/NetworkProfilesPresenterTest.java
@@ -1,14 +1,20 @@
 package com.vishesh.tpc_stud.networkProfiles.presenters;
 
+import com.vishesh.tpc_stud.core.repos.LocalCache;
 import com.vishesh.tpc_stud.networkProfiles.useCases.GetNetworkProfilesUseCase;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import io.reactivex.observers.DisposableSingleObserver;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,24 +24,28 @@ public class NetworkProfilesPresenterTest {
 
     @Mock
     private NetworkProfilesPresenter.NetworkProfilesView networkProfilesView;
-
     @Mock
     private GetNetworkProfilesUseCase getNetworkProfilesUseCase;
+    @Mock
+    private LocalCache localCache;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
 
-        networkProfilesPresenter = new NetworkProfilesPresenter(getNetworkProfilesUseCase);
-
+        networkProfilesPresenter = new NetworkProfilesPresenter(getNetworkProfilesUseCase,
+                localCache);
+        networkProfilesPresenter.setNetworkProfilesView(networkProfilesView);
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void onStart_getNetworkProfiles() {
         networkProfilesPresenter.onStart();
 
         verify(networkProfilesView).showLoader();
-
+        verify(getNetworkProfilesUseCase).execute(any(DisposableSingleObserver.class),
+                anyInt(), Mockito.any(Object.class));
     }
 
 }
